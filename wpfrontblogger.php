5d366ec973f1eb8014a457848b99ac2f700af52a
Maurice [Code045NL] <maurice@code045.nl>
2025-07-22 01:34:17 +0000

    Bump version to 1.0.12 in plugin header

--- wpfrontblogger.php
+++ wpfrontblogger.php
@@ -5,7 +5,7 @@
  * Description:Post Blogs to your WordPress site with ease using the wpfrontblogger plugin.
  * Author: WeAreCode045
  * Author URI: https://code045.nl/
- * Version: 1.0.11
+ * Version: 1.0.12
  * Requires at least: 6.0
  * Tested up to: 6.7
  *
